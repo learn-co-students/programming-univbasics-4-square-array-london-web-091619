9146d4d6387dd74d1f6ff90ffbaef814006cb73a
Luke Prendergast <luke.prendergast94@gmail.com>
2019-09-08 18:58:39 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+count = 0
+result = []
+while count < array.length do
+  result.unshift(array[count] * array[count])
+  count +=1
+end
+return result.sort
 end
